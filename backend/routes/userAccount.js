eca13d1163de27e799e47459a82e77e10989f387
Owen Hall <owen9hall@gmail.com>
2025-10-10 22:42:01 -0700

    registration schema validation

--- backend/routes/userAccount.js
+++ backend/routes/userAccount.js
@@ -1,8 +1,52 @@
 const express = require('express');
 const app = express();
+app.use(express.json())
+const Joi = require('joi'); // library for defining schemas for json schema validation
 
-// All userAccount endpoints go here
+const userAccountService = require('../services/userAccountService'); // import userAccountService which contains all business logic
 
+
+// GET endpoint for testing
 module.exports = app.get('/', (req, res)=>{
     res.status(200).send('Test GET request from userAccount.js file');
+});
+
+// user registration schema
+const registrationSchema = Joi.object({
+    first_name: Joi.string()
+        .min(1)
+        .max(30)
+        .pattern(new RegExp('^[A-Za-z]+$'))
+        .required(),
+
+    last_name: Joi.string()
+        .min(1)
+        .max(30)
+        .pattern(new RegExp('^[A-Za-z]+$'))
+        .required(),
+    
+    email: Joi.string()
+        .email()
+        .required(),
+    
+    password: Joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+        .required(),
+
+    repeat_password: Joi.string()
+        .required()
+        .valid(Joi.ref('password'))
+        .messages({ 'any.only': 'repeat_password must match password' }),
+}).required();
+
+// POST endpoint for registering a user account
+module.exports = app.post('/register', (req, res)=> {
+    // validate schema
+    const { error, value } = registrationSchema.validate(req.body);
+    if (error) {
+        return res.status(400).json({ error: "Invalid request schema: " + error.details[0].message });
+    }
+    const [ status, response ] = userAccountService.registerUser(req.body);
+    
+    return res.status(status).json(response);
 });
--- backend/services/userAccountService.js
+++ backend/services/userAccountService.js
@@ -1,1 +1,15 @@
 // All business logic for userAccount endpoints goes here
+
+function registerUser(body) {
+    // check if account already exists
+
+    // register new account
+
+
+    
+    return [200, "New account successfully created"];
+}
+
+module.exports = {
+    registerUser,
+};
